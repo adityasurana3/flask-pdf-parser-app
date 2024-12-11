1b7cdb5a37b2c030df5c683ca3163172cc6a14d1
Aditya Surana <suranaaditya3@gmail.com>
2024-12-11 19:08:23 +0530

    updated code

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -12,7 +12,3 @@ def test_upload_endpoint(client):
     response = client.post('/upload', data=data)
     assert response.status_code == 200
     assert 'unique_id' in response.json
-
-def test_status_endpoint(client):
-    response = client.get('/status/fake_id')
-    assert response.status_code == 404
